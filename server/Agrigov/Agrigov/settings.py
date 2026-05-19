f7bb949f7e3e0c919e7de79b4a0575a8c77c15dc
Djawad Dabbache <djawadmne@gmail.com>
2026-05-19 20:38:59 +0100

    minor change(policies)

--- server/Agrigov/Agrigov/settings.py
+++ server/Agrigov/Agrigov/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME"),
